c90661b0174c01ee834bf2c48ff6fcb24128436e
Mrunal Patel <mrunal@me.com>
2015-01-15 19:58:08 -0800

    Make setup more re-entrant

--- subnet/setup.sh
+++ subnet/setup.sh
@@ -7,12 +7,14 @@ ovs-vsctl set bridge br0 protocols=OpenFlow13
 ovs-vsctl del-port br0 vxlan0 || true
 ovs-vsctl add-port br0 vxlan0 -- set Interface vxlan0 type=vxlan options:remote_ip="flow" options:key="flow" ofport_request=10
 
+ip link del vlinuxbr || true
 ip link add vlinuxbr type veth peer name vovsbr
 ip link set vlinuxbr up
 ip link set vovsbr up
 ip link set vlinuxbr txqueuelen 0
 ip link set vovsbr txqueuelen 0
 
+ovs-vsctl del-port br0 vovsbr || true
 ovs-vsctl add-port br0 vovsbr -- set Interface vovsbr ofport_request=9
 
 cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-lbr0
