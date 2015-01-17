70924ac433e07c844e6a313fc06b39c549495dba
Mrunal Patel <mrunal@me.com>
2015-01-16 20:53:31 -0800

    Parametrize setup script

--- subnet/setup.sh
+++ subnet/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+bridge_ip=$1 //10.244.3.1
+bridge_mask=$2 //255.255.255.0
+base_network=$3 //10.244.0.0/16
+
 ovs-vsctl del-br br0 || true
 ovs-vsctl add-br br0 -- set Bridge br0 fail-mode=secure
 ovs-vsctl set bridge br0 stp_enable=true
@@ -22,8 +26,8 @@ DEVICE=lbr0
 ONBOOT=yes
 TYPE=Bridge
 BOOTPROTO=static
-IPADDR=10.244.3.1
-NETMASK=255.255.255.0
+IPADDR=${bridge_ip}
+NETMASK=${bridge_mask}
 STP=yes
 EOF
 
@@ -31,5 +35,5 @@ service network restart
 
 brctl addif lbr0 vlinuxbr
 
-ip route del 10.244.3.0/24 dev lbr0  proto kernel  scope link  src 10.244.3.1
-ip route add 10.244.0.0/16 dev lbr0  proto kernel  scope link  src 10.244.3.1
+//ip route del 10.244.3.0/24 dev lbr0  proto kernel  scope link  src 10.244.3.1
+ip route add $base_network dev lbr0  proto kernel  scope link  src $bridge_ip
